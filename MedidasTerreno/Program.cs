4c495364652f31810df680dcc5b21333d5f0ffea
Tiago Dos Santos Cordeiro <86391368+Tiagosancor@users.noreply.github.com>
2022-05-18 00:23:34 -0300

    Adicionar arquivos de projeto.

---
+++ MedidasTerreno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace MedidasTerreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terreno terreno = new Terreno();
+
+            Console.WriteLine("Informe a Largura do terreno:");
+            terreno.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Informe o comprimento do terreno:");
+            terreno.Comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Informe o preço por metro quadrado:");
+            terreno.PrecoMtq = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(terreno);
+            
+        }
+    }
+}
---
+++ MedidasTerreno/Terreno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidasTerreno
+{
+    class Terreno
+    {
+        public Terreno()
+        {
+        }
+
+        public double Largura { get; set; }
+        public double Comprimento { get; set; }
+        public double PrecoMtq { get; set; }
+        public double Area { get; set; }
+        
+        public double AreaTotal()
+        {
+            return Largura * Comprimento;
+        }
+
+        public double PrecoTotal()
+        {
+            return AreaTotal() * PrecoMtq;
+        }
+
+        public override string ToString()
+        {
+            return "AREA = " + AreaTotal().ToString("F2", CultureInfo.InvariantCulture)
+                + "\n"
+                + "PRECO = " + PrecoTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
